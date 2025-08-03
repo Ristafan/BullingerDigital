17629f5750679dac71d31fa3687e302f994de338
Ristafan <martin.faehnrich@gmail.com>
2025-08-03 22:21:31 +0200

    Feat: Fine-tune GLiNER

--- src/GLiNER/FineTuner.py
+++ src/GLiNER/FineTuner.py
@@ -2,6 +2,7 @@ import json
 import random
 import os
 import torch
+from contextlib import redirect_stdout
 from gliner import GLiNERConfig, GLiNER
 from gliner.training import Trainer, TrainingArguments
 from gliner.data_processing.collator import DataCollatorWithPadding, DataCollator
@@ -12,19 +13,13 @@ os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 print("GLiNER FineTuner")
 
-train_path = "data.json"
+train_path = "training.json"
 
-with open(train_path, "r") as f:
+with open(train_path, "r", encoding="utf-8") as f:
     data = json.load(f)
 
 print('Dataset size:', len(data))
-
 print(data[1])  # Display the first item in the dataset to understand its structure
-
-print(data[1]["tokenized_text"][17])
-print(data[1]["tokenized_text"][18])
-print(data[1]["tokenized_text"][19])
-
 
 random.shuffle(data)
 print('Dataset is shuffled...')
@@ -36,14 +31,14 @@ print('Dataset is splitted...')
 
 device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
-model = GLiNER.from_pretrained("urchade/gliner_small")
+model = GLiNER.from_pretrained("knowledgator/gliner-x-large")
 
 # use it for better performance, it mimics original implementation but it's less memory efficient
 data_collator = DataCollator(model.config, data_processor=model.data_processor, prepare_labels=True)
 
 # Optional: compile model for faster training
 model.to(device)
-print("done")
+print("model to device done")
 
 # calculate number of epochs
 num_steps = 500
@@ -80,18 +75,12 @@ trainer = Trainer(
     data_collator=data_collator,
 )
 
+print("Start Training...")
+
 trainer.train()
 
-trained_model = GLiNER.from_pretrained("models/checkpoint-100", load_tokenizer=True)
+# Save the trained model
+trainer.save_model("models/trained_model.pth")
 
-text = "Cristiano Ronaldo dos Santos Aveiro (Portuguese pronunciation: [kɾiʃˈtjɐnu ʁɔˈnaldu]; born 5 February 1985) is a Portuguese professional footballer who plays as a forward for and captains both Saudi Pro League club Al Nassr and the Portugal national team. Widely regarded as one of the greatest players of all time, Ronaldo has won five Ballon d'Or awards,[note 3] a record three UEFA Men's Player of the Year Awards, and four European Golden Shoes, the most by a European player. He has won 33 trophies in his career, including seven league titles, five UEFA Champions Leagues, the UEFA European Championship and the UEFA Nations League. Ronaldo holds the records for most appearances (183), goals (140) and assists (42) in the Champions League, goals in the European Championship (14), international goals (128) and international appearances (205). He is one of the few players to have made over 1,200 professional career appearances, the most by an outfield player, and has scored over 850 official senior career goals for club and country, making him the top goalscorer of all time."
-
-# Labels for entity prediction
-labels = ["Person", "Award"] # for v2.1 use capital case for better performance
-
-# Perform entity prediction
-entities = trained_model.predict_entities(text, labels, threshold=0.5)
-
-# Display predicted entities and their labels
-for entity in entities:
-    print(entity["text"], "=>", entity["label"])
+with open('out.txt', 'w') as f:
+    f.write("Training completed successfully!\n")
