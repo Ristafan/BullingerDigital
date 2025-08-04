f31c5e8ee795e6f3f9e26adc2a9ed68e31929f31
Ristafan <martin.faehnrich@gmail.com>
2025-08-04 07:36:11 +0200

    Feat: NER save to JSON

--- src/GLiNER/NER.py
+++ src/GLiNER/NER.py
@@ -41,46 +41,40 @@ class NER:
     def predict(self, text, labels=["Location", "Person"], threshold=0.5):
         return self.model.predict_entities(text, labels, threshold)
 
-    def display(self, entities):
-        for entity in entities:
-            print(entity["text"], "=>", entity["label"])
+    def mapping_to_token_indices(self, entity, data):
+        start_char = entity['start']
+        end_char = entity['end']
 
+        # Map character positions to token indices
+        token_start, token_end = None, None
+        for i, token in enumerate(data[sentence_pair]['tokenized_text']):
+            token_index, token_text = token.split("_", 1)
+            token_index = int(token_index)
 
-if __name__ == "__main__":
-    with open("training_sentences_and_annotations(1).json", "r", encoding="utf-8") as f:
-        data = json.load(f)
+            # Get start and end positions of token in original sentence
+            token_char_start = original_sentence.find(token_text)
+            token_char_end = token_char_start + len(token_text)
 
-    ner = NER()
+            if token_char_start <= start_char < token_char_end:
+                token_start = token_index
+            if token_char_start < end_char <= token_char_end:
+                token_end = token_index
 
-    for sentence in tqdm(data.keys()):
+        return token_start, token_end
+
+    @staticmethod
+    def save_to_txt(sentence_pair, data, entities):
         with open("predicted_entities.txt", "a", encoding="utf-8") as f:
 
-            f.write(f"Processing sentence: {sentence}\n")
-            original_sentence = data[sentence]["original_sentence"]
-            entities = ner.predict(original_sentence, labels=["Location", "Person"], threshold=0.5)
+            f.write(f"Processing sentence: {sentence_pair}\n")
 
-            f.write(f"Original Sentence: {" ".join(data[sentence]['tokenized_text'])}\n")
+            f.write(f"Original Sentence: {" ".join(data[sentence_pair]['tokenized_text'])}\n")
             f.write("Labelled Entities:\n")
-            f.write(str(data[sentence]["ner"]))
+            f.write(str(data[sentence_pair]["ner"]))
             f.write("\nPredicted Entities:\n")
+
             for entity in entities:
-                start_char = entity['start']
-                end_char = entity['end']
-
-                # Map character positions to token indices
-                token_start, token_end = None, None
-                for i, token in enumerate(data[sentence]['tokenized_text']):
-                    token_index, token_text = token.split("_", 1)
-                    token_index = int(token_index)
-
-                    # Get start and end positions of token in original sentence
-                    token_char_start = original_sentence.find(token_text)
-                    token_char_end = token_char_start + len(token_text)
-
-                    if token_char_start <= start_char < token_char_end:
-                        token_start = token_index
-                    if token_char_start < end_char <= token_char_end:
-                        token_end = token_index
+                token_start, token_end = ner.mapping_to_token_indices(entity, data)
 
                 if token_start is not None and token_end is not None:
                     f.write(f"[{token_start}, {token_end}, {entity['label']}] --> {entity['text']} score: {round(entity['score'], 2)}\n")
@@ -90,3 +84,52 @@ if __name__ == "__main__":
             f.write("\n")
             f.write("=" * 50 + "\n")
             f.write("\n")
+
+    @staticmethod
+    def save_to_json(sentence_pair, original_sentence, entities):
+        output = {
+            "sentence_pair": sentence_pair,
+            "original_sentence": original_sentence,
+            "labelled_entities": data[sentence_pair]["ner"],
+            "entities": entities
+        }
+
+        return output
+
+
+if __name__ == "__main__":
+    all_predicted_entities = []
+
+    # Load the data from the JSON file
+    with open("training_sentences_and_annotations(1).json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    # Initialize the NER model
+    ner = NER()
+    i = 0
+
+    # Process each sentence pair in the data
+    for sentence_pair in tqdm(data.keys()):
+        # Predict entities for each sentence pair
+        original_sentence = data[sentence_pair]["original_sentence"]
+        entities = ner.predict(original_sentence, labels=["Location", "Person"], threshold=0.5)
+
+        # Save the results to a text file
+        ner.save_to_txt(sentence_pair, data, entities)
+
+        # Save the results to later use in a JSON file
+        all_predicted_entities.append(ner.save_to_json(sentence_pair, original_sentence, entities))
+
+        # Save the results to later us in an Excel file
+
+
+
+        i += 1
+        if i == 20:
+            break
+
+    # Save all predicted entities to a JSON file
+    with open("predicted_entities.json", "a", encoding="utf-8") as f:
+        json.dump(all_predicted_entities, f, ensure_ascii=False, indent=4)
+
+
